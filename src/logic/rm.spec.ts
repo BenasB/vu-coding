8ad564cffb80577ca87db29834dfd56f94277155
BenasB <29711974+BenasB@users.noreply.github.com>
2023-12-06 21:15:54 +0200

    Add dog food tests

--- src/logic/rm.spec.ts
+++ src/logic/rm.spec.ts
@@ -1,5 +1,4 @@
 import { createBinaryString } from '../utils/type-utils';
-import { BinaryString } from '../utils/types';
 import {
   reedMullerControlMatrix,
   reedMullerDecode,
@@ -9,23 +8,67 @@ import {
   reedMullerGenerationMatrix,
 } from './encoding/rmEncoding';
 import { Matrix } from './math/matrix';
+import fc from 'fast-check';
 
+// This is a property test
 // A value that gets encoded, must be decoded to the same value
-describe('reedMuller dogfood', () => {
-  it('RM(1, 3) example 1', () => {
-    const m = 3;
-    const generationMatrix: Matrix = reedMullerGenerationMatrix(1, m);
-    const controlMatrices: Matrix[] = [...(Array(m) as undefined[])].map(
-      (_, i) => reedMullerControlMatrix(i + 1, m),
-    );
+test('reedMuller dogfood property check', () => {
+  fc.assert(
+    fc.property(fc.nat({ max: 9 }), r => {
+      const m = r + 1; // m value will be [1; 10]
+      const generationMatrix: Matrix = reedMullerGenerationMatrix(1, m);
+      const controlMatrices: Matrix[] = [...(Array(m) as undefined[])].map(
+        (_, i) => reedMullerControlMatrix(i + 1, m),
+      );
 
-    const input: BinaryString = createBinaryString('11011101111011111011');
-    expect(
-      reedMullerDecode(
-        reedMullerEncode(input, m, generationMatrix),
-        controlMatrices,
-        m,
-      ),
-    ).toEqual(input);
-  });
+      fc.assert(
+        fc.property(
+          fc.stringOf(fc.constantFrom('0', '1'), {
+            maxLength: 10000,
+            minLength: 1,
+          }),
+          input => {
+            const paddedInput = input.padEnd(
+              input.length + ((m + 1 - (input.length % (m + 1))) % (m + 1)),
+              '0',
+            );
+
+            return expect(
+              reedMullerDecode(
+                reedMullerEncode(
+                  createBinaryString(paddedInput),
+                  m,
+                  generationMatrix,
+                ),
+                controlMatrices,
+                m,
+              ),
+            ).toEqual(paddedInput);
+          },
+        ),
+        { numRuns: 20 },
+      );
+    }),
+    { numRuns: 10 },
+  );
+});
+
+test('reedMuller dogfood manual', () => {
+  const m = 3;
+  const generationMatrix: Matrix = reedMullerGenerationMatrix(1, m);
+  const controlMatrices: Matrix[] = [...(Array(m) as undefined[])].map((_, i) =>
+    reedMullerControlMatrix(i + 1, m),
+  );
+  const input = '011101110110111';
+  const paddedInput = input.padEnd(
+    input.length + ((m + 1 - (input.length % (m + 1))) % (m + 1)),
+    '0',
+  );
+  expect(
+    reedMullerDecode(
+      reedMullerEncode(createBinaryString(paddedInput), m, generationMatrix),
+      controlMatrices,
+      m,
+    ),
+  ).toEqual(paddedInput);
 });
